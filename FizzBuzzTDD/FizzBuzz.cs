6ed2144d24645590e7aa326b77b13093867db0e2
Jafca <Jafca@users.noreply.github.com>
2023-10-25 16:35:50 +0100

    Added Buzz code - tests pass.

--- FizzBuzzTDD/FizzBuzz.cs
+++ FizzBuzzTDD/FizzBuzz.cs
@@ -7,6 +7,9 @@
             if (number % 3 == 0)
                 return "Fizz";
 
+            if (number % 5 == 0)
+                return "Buzz";
+
             return number.ToString();
         }
     }

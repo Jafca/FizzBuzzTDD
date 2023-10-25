f53868ef70e2763723272fb8f5fb78646b9c0896
Jafca <Jafca@users.noreply.github.com>
2023-10-25 17:01:05 +0100

    Get Rule types with Reflection.

--- FizzBuzzTDD/FizzBuzz.cs
+++ FizzBuzzTDD/FizzBuzz.cs
@@ -1,16 +1,25 @@
-﻿namespace FizzBuzzTDD
+﻿using System.Reflection;
+
+namespace FizzBuzzTDD
 {
     public class FizzBuzz
     {
         public string Run(int number)
         {
             var result = string.Empty;
+            var rules = new List<IRule>();
 
-            var rules = new List<IRule>
+            var ruleTypes = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(type => typeof(IRule).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract);
+
+            foreach (var type in ruleTypes)
             {
-                new FizzRule(),
-                new BuzzRule(),
-            };
+                var instance = Activator.CreateInstance(type);
+                if (instance != null)
+                {
+                    rules.Add((IRule)instance);
+                }
+            }
 
             foreach (var rule in rules)
             {
